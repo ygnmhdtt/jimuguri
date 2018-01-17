0980f110378140960e471768259bf02f7d79d782
ygnmhdtt <yaginuma@mmmcorp.co.jp>
2018-01-17 21:44:42 +0900

    add argument

--- lib/jimuguri/cli.rb
+++ lib/jimuguri/cli.rb
@@ -11,15 +11,27 @@ class Cli
     @cmds = {}
     @helps = {}
     init_help
+    init_version
+    @opt = OptionParser.new
+    @options = {}
   end
 
   # auto generate help
   def init_help
     help_proc = Proc.new do
-      puts help
+      print_help
     end
     @cmds[:help] = help_proc
     @helps[:help] = 'Shows a list of commands or help for one command'
+  end
+
+  # auto generate version
+  def init_version
+    version_proc = Proc.new do
+      print_version
+    end
+    @cmds[:version] = version_proc
+    @helps[:version] = 'Shows version'
   end
 
   # add action by given block
@@ -31,12 +43,22 @@ class Cli
     @helps[cmd.to_sym] = help
   end
 
+  def add_option(short, long, message)
+    @opt.on("-#{short}", "--#{long}", message) {|v| @options[long.split(' ')[0].to_sym] = v}
+    puts long.to_sym
+  end
+
   def run(arg)
+    # disable optparse default help, version
+    help if arg.include?('-h') || arg.include?('--help')
+    version if arg.include?('-v') || arg.include?('--version')
+
+    # parse
+    help if !@opt.parse(arg)
+
     action = @cmds[arg[0].to_sym]
-    if action.nil?
-      puts help
-      return
-    end
+    help if action.nil?
+
     action.call
   end
 
@@ -54,6 +76,20 @@ class Cli
 
   def version
     @version
+  end
+
+  def options
+    @options
+  end
+
+  def print_version
+    puts version
+    exit(0)
+  end
+
+  def print_help
+    puts help
+    exit(0)
   end
 
   def help
